ff0748f37dfa978e2f7919fcd734978c1c369574
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-12-04 00:24:27 +0700

    Add react-hook-form for form handling and update App component structure

--- fullstack_test/src/App.jsx
+++ fullstack_test/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Text } from "@shopify/polaris";
+import { useFieldArray, useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { control, register, watch, handleSubmit } = useForm({
+    defaultValues: {
+      campaignName: "",
+      optioms: [
+        { title: "", quantity: 1, discountType: "None", amount: "" },
+        { title: "", quantity: 2, discountType: "None", amount: "" },
+      ],
+    },
+  });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "options",
+  });
+
+  return <Text> This is my test</Text>;
 }
 
-export default App
+export default App;
