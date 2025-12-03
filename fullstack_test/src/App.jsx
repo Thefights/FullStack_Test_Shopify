712bd2b702f73bb1cd1292f4b4e34d91bd9bf9d2
phuckhang1088@gmail.com <phuckhang1088@gmail.com>
2025-12-04 01:27:01 +0700

    Refactor code structure for improved readability and maintainability

--- fullstack_test/src/App.jsx
+++ fullstack_test/src/App.jsx
@@ -1,14 +1,30 @@
-import { Text } from "@shopify/polaris";
+import { Layout, LegacyCard, Page } from "@shopify/polaris";
+import "@shopify/polaris/build/esm/styles.css";
 import { useFieldArray, useForm } from "react-hook-form";
-import "./App.css";
 
 function App() {
   const { control, register, watch, handleSubmit } = useForm({
     defaultValues: {
-      campaignName: "",
+      campaign: "",
+      title: "",
+      description: "",
       optioms: [
-        { title: "", quantity: 1, discountType: "None", amount: "" },
-        { title: "", quantity: 2, discountType: "None", amount: "" },
+        {
+          title: "Single",
+          subTitle: "Standard price",
+          label: "",
+          quantity: 1,
+          discountType: "None",
+          amount: "",
+        },
+        {
+          title: "Duo",
+          subTitle: "Save 10%",
+          quantity: 2,
+          label: "Popular",
+          discountType: "% discount",
+          amount: "10%",
+        },
       ],
     },
   });
@@ -18,7 +34,35 @@ function App() {
     name: "options",
   });
 
-  return <Text> This is my test</Text>;
+  return (
+    <Page fullWidth title="Create volume discount">
+      <Layout>
+        <Layout.Section>
+          <LegacyCard title="Order details" sectioned>
+            <p>
+              Use to follow a normal section with a secondary section to create
+              a 2/3 + 1/3 layout on detail pages (such as individual product or
+              order pages). Can also be used on any page that needs to structure
+              a lot of content. This layout stacks the columns on small screens.
+            </p>
+          </LegacyCard>
+          <LegacyCard title="Order details" sectioned>
+            <p>
+              Use to follow a normal section with a secondary section to create
+              a 2/3 + 1/3 layout on detail pages (such as individual product or
+              order pages). Can also be used on any page that needs to structure
+              a lot of content. This layout stacks the columns on small screens.
+            </p>
+          </LegacyCard>
+        </Layout.Section>
+        <Layout.Section variant="oneThird">
+          <LegacyCard title="Tags" sectioned>
+            <p>Add tags to your order.</p>
+          </LegacyCard>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
 }
 
 export default App;
